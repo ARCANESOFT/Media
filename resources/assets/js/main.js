dfd2fd99482c3af285d334c318122e4568c1f0cf
ARCANEDEV <arcanedev.maroc@gmail.com>
2017-04-25 13:49:28 +0100

    Updating the assets

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,12 +1,12 @@
-import Vue from 'vue';
-
-if ( ! window.eventHub) {
-    window.eventHub = new Vue();
-}
-
 /**
  * Register Vue components...
  */
+import Vue from 'vue';
+
+if ( ! window.eventHub) {
+    window.eventHub = new Vue;
+}
+
 Vue.component('media-manager',       require('./MediaManager.vue'));
 Vue.component('media-browser',       require('./MediaBrowser.vue'));
-Vue.component('media-browser-modal', require('./Components/Modals/BrowseMediaModal.vue'));
+Vue.component('media-browser-modal', require('./Modals/BrowseMediaModal.vue'));
