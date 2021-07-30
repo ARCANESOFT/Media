280da8e2be841791fe0f758816e5d9849c0f3b7c
ARCANEDEV <arcanedev.maroc@gmail.com>
2021-07-30 01:41:59 +0100

    WIP

--- views/index.blade.php
+++ views/index.blade.php
@@ -1,12 +1,7 @@
-@extends(arcanesoft\foundation()->template())
+<x-arc:layout>
+    @section('page-title')
+        <i class="far fa-fw fa-images"></i> @lang('Media')
+    @endsection
 
-@section('page-title')
-    <i class="far fa-fw fa-images"></i> @lang('Media')
-@endsection
-
-@section('content')
     <v-media-manager></v-media-manager>
-@endsection
-
-@push('scripts')
-@endpush
+</x-arc:layout>
